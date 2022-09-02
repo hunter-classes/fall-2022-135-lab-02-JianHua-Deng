6de9b31219a3a36c01eacc1ffac1b46fb22c8bc9
Metails <dengjianhua2397462613@gmail.com>
2022-09-02 09:40:33 -0400

    Added more test condition in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@ int main()
 {	
 	
 	print_interval(2, 9);
+	std::cout << std::endl;
+
+	print_interval(-10, 5);
+	std::cout << std::endl;
+
+	print_interval(90, 110);
+	std::cout << std::endl;
+
+	print_interval(40, 60);
+	std::cout << std::endl;
+
+	print_interval(-12, 0);
+	std::cout << std::endl;
 
 	return 0;
 }//end main
